6d0031307868be6c9556cd727fa9c29af0944d5c
Jason May <jason.a.may@gmail.com>
2014-08-07 19:40:07 -0400

    get basic socket listening going

--- src/main/java/org/jarsonmar/awesomud/Listener.java
+++ src/main/java/org/jarsonmar/awesomud/Listener.java
@@ -1,7 +1,61 @@
 package org.jarsonmar.awesomud;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 class Listener {
-    public static void main(String[] args) {
-    	System.err.println("ok");
-    }
+
+   public static class ListenThread implements Runnable {
+      private final Socket sock;
+
+      public ListenThread(Socket sock) {
+         this.sock = sock;
+      }
+
+      @Override
+      public void run() {
+         try (BufferedReader reader = new BufferedReader(
+               new InputStreamReader(sock.getInputStream()));
+               PrintWriter writer = new PrintWriter(
+                     sock.getOutputStream(), true)) {
+            writer.println("[f you]");
+
+            int n = 0;
+            while (n < 10) {
+               String line = reader.readLine();
+               if (null == line) {
+                  System.err.println("disconnect!");
+                  break;
+               }
+               System.err.println("Yo. Someone said:");
+               System.err.println(line.trim());
+               System.err.println("At time: " + n);
+               System.err.println();
+               n++;
+            }
+            writer.println("you are done!");
+         } catch (IOException e) {
+            System.err.println(e.getMessage());
+         }
+      }
+
+   }
+
+   public static void main(String[] args) {
+      try (ServerSocket srv = new ServerSocket(6725)) {
+         System.err.println("My Java is better than yours");
+
+         while (true) {
+            Socket sock = srv.accept();
+            ListenThread l = new ListenThread(sock);
+            new Thread(l).start();
+         }
+      } catch (IOException e) {
+         System.err.println("disconnect?");
+      }
+   }
 }
