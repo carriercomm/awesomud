3d02e7df74a1bd776fbc6ceceba38c9c2aa46a52
Jason May <jason.a.may@gmail.com>
2014-09-01 18:46:02 -0400

    move the mobiles around in redis space on the timers

--- mud.js
+++ mud.js
@@ -3,7 +3,10 @@ var redis = require('redis'),
     events = require('events'),
     async = require('async'),
     _ = require('lodash'),
-    rnorm = require('randgen').rnorm;
+    randgen = require('randgen'),
+    rnorm = randgen.rnorm,
+    runif = randgen.runif
+    ;
 
 /*
  * NOTE: early POC stages!
@@ -14,13 +17,103 @@ var ns = 'org.jarsonmar.awesomud';
 var mobiles = [];
 var timers = {};
 
-var seenMove = {};
 var eventEmitter = new events.EventEmitter();
 
 function getMobSpeed(mobkey) {
     var rkey = [ns,'mob',mobkey,'properties'].join(':');
     client.hget(rkey, 'speed', function(err, speed) {
         setTimer(mobkey, parseInt(speed));
+    });
+}
+
+function randomDirection(directions) {
+    var len = directions.length;
+    var idx = runif(0, len, true);
+    return directions[idx];
+}
+
+function moveMobile(mobkey, callback) {
+
+    var from, to, dir;
+    var rkey = [ns,'mob',mobkey,'properties'].join(':');
+    var getLocation = function(callback) {
+        client.hget(rkey, 'location', function(err, location) {
+            from = location;
+            callback();
+        });
+    };
+
+    var getDirection = function(callback) {
+        var fromExitsKey = [ns,'loc',from,'exits'].join(':');
+        client.hkeys(fromExitsKey, function(err, directions) {
+            dir = randomDirection(directions);
+
+            // NOTE: This is where door handling is IMPORTANT
+            if (directions && directions.length > 0) {
+                callback();
+            }
+            else {
+                console.log(from + " has no exits! abandoning movement timer");
+                delete timers[mobkey];
+            }
+        });
+    };
+
+    var getDestination = function(callback) {
+        client.hget([ns,'loc',from,'exits'].join(':'), dir, function(err, destination) {
+            to = destination;
+            if (err || !to) {
+                console.error(err);
+            }
+            else {
+                callback();
+            }
+        });
+    };
+
+    var updateLocationProperty = function(callback) {
+        client.hset(rkey, 'location', to, function(err) {
+            if (err) {
+                console.error(err);
+            }
+            else {
+                callback();
+            }
+        });
+    };
+    
+    var updateLocationSets = function(callback) {
+        var src = [ns,'loc',from,'mobs'].join(':');
+        var dst = [ns,'loc',to,'mobs'].join(':');
+        client.smove(src, dst, mobkey, function(err) {
+            if (err) {
+                console.error(err);
+            }
+            callback();
+        });
+    };
+
+    var transferLocation = function(callback) {
+        async.parallel(
+            [
+                updateLocationProperty,
+                updateLocationSets,
+            ],
+            function() {
+                callback();
+            }
+        );
+    };
+
+    var actionsInSeries = [
+        getLocation,
+        getDirection,
+        getDestination,
+        transferLocation,
+    ];
+
+    async.series(actionsInSeries, function() {
+        callback(mobkey, dir, from, to);
     });
 }
 
@@ -33,13 +126,23 @@ function setTimer(mobkey, speed) {
         gaussesGuess = rnorm(meantime, meantime * 0.25);
     } while (gaussesGuess < 0);
 
+    // Set the timer to a defined value so we can
+    // check for deletedness later
+    timers[mobkey] = true;
+
+    // Set the timer mode
     timers[mobkey] = setTimeout(function() {
-        if (!seenMove[mobkey]) {
-            seenMove[mobkey] = true;
-            console.log(_.keys(seenMove).length);
+        moveMobile(mobkey, function(key, dir, from, to) {
+            if (mobkey === 'treadmill:speed') {
+                console.log("move " + mobkey + ' [' + dir + '] (' + from + ' -> ' + to + ')');
+            }
+        });
+
+        // timer key may have been deleted for other reasons
+        // whatever reason that may be, just leave it be
+        if (timers[mobkey]) {
+            timers[mobkey] = setTimer(mobkey, speed);
         }
-        console.log("move " + mobkey + ' (' + gaussesGuess + ')');
-        timers[mobkey] = setTimer(mobkey, speed);
     }, gaussesGuess * 2000);
 }
 
