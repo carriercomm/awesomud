466d8fad18d50feb5db63552850f9211fb2833e3
Jason May <jason.a.may@gmail.com>
2014-09-01 12:15:42 -0400

    set up timers over a normal distribution

--- mud.js
+++ mud.js
@@ -1,32 +1,46 @@
 var redis = require('redis'),
-    client = redis.createClient();
+    client = redis.createClient(),
+    events = require('events'),
+    async = require('async'),
+    _ = require('lodash'),
+    rnorm = require('randgen').rnorm;
 
 var ns = 'org.jarsonmar.awesomud';
 
 var mobiles = [];
+var timers = {};
+
+var seenMove = {};
+var eventEmitter = new events.EventEmitter();
+
+function getMobSpeed(mobkey) {
+    var rkey = [ns,'mob',mobkey,'properties'].join(':');
+    client.hget(rkey, 'speed', function(err, speed) {
+        setTimer(mobkey, parseInt(speed));
+    });
+}
+
+function setTimer(mobkey, speed) {
+    if (speed === 0) return;
+
+    var gaussesGuess;
+    var meantime = (100 / speed);
+    do {
+        gaussesGuess = rnorm(meantime, meantime * 0.25);
+    } while (gaussesGuess < 0);
+
+    timers[mobkey] = setTimeout(function() {
+        if (!seenMove[mobkey]) {
+            seenMove[mobkey] = true;
+            console.log(_.keys(seenMove).length);
+        }
+        console.log("move " + mobkey + ' (' + gaussesGuess + ')');
+        timers[mobkey] = setTimer(mobkey, speed);
+    }, gaussesGuess * 2000);
+}
 
 client.smembers(ns + ":movables", function(err, movables) {
-    var mlen = movables.length;
-    var count = 0;
-    for (var i = 0; i < mlen; i++) {
-        var mobkey = movables[i];
-        //client.hmget(
-        //    ns + ":mob:" + mobkey + ":properties",
-        //    "name",
-        //    "location",
-        //    "speed", function(err, props) {
-        //    //var mob = {
-        //    //    name: props[0],
-        //    //    location: props[1],
-        //    //    speed: parseInt(props[2]),
-        //    //};
-        //    //mobiles.push(mob);
-        //    count++; // So we know when we're on the last event
-
-        //    if (count === mlen) {
-        //        
-        //    }
-        //});
-        
-    }
+    async.forEach(movables, function(movable, callback) {
+        getMobSpeed(movable);
+    });
 });
